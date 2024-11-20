3a433e4314b44efde149c084fae9bb3d0712814c
Ronaldo Venâncio da Silva <95707584+ronaldovenanciorvs@users.noreply.github.com>
2024-11-20 14:17:31 -0300

    upload files

--- server.js
+++ server.js
@@ -8,9 +8,14 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
-const app = express();
 const static = require("./routes/static");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
+const Util = require("./utilities/");
+const utilities = require("./utilities/");
+const pool = require("./database/");
 
+const app = express();
 
 /* ****************************
  * View Engine and Templates
@@ -19,16 +24,39 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // not at views root
 
-
 /* ***********************
  * Routes
  *************************/
 app.use(static);
 // Index route
-app.get ("/", function(req, res){
-  res.render("index", {title: "Home"});
-});
 
+//app.get("/", baseController.buildHome);
+app.get("/", Util.handleErrors(baseController.buildHome));
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// Inventory routes
+app.use("/inv", inventoryRoute);
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+utilities.handleErrors(baseController.buildHome);
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
@@ -41,5 +69,5 @@ const host = process.env.HOST;
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`);
+  console.log(`app listening on ${host}:${port}`)
 });
